0ab967d5afd8f3db428b980a00579eb257368600
KhushiAdhaduk <kdadhaduk2004@gmail.com>
2024-03-15 10:32:47 +0530

    changes iin landing page

--- src/view/home/Landing.js
+++ src/view/home/Landing.js
@@ -16,11 +16,36 @@ const Landing = () => {
 export default Landing;
 
 const Container = styled.div`
-  z-index: 1;
-  background-image: url("./img/bg_img.jpg");
+  // background-image: url("./img/bg_img.jpg");
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 20px;
+  text-align: center;
   min-height: 70vh;
   background-size: cover;
-  padding: 153px 0;
+  // padding: 280px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 90%;
+  z-index: -1;
+  background: linear-gradient(-45deg, #585fa3, #cebe61, #5e5d8c, #1d006b);
+  background-size: 400% 400%;
+  animation: gradient 10s ease infinite;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 `;
 
 const Title = styled.h2`
